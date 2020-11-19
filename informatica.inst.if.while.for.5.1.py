6d724261fb62a0591432ae000ecfe3168a84e0f8
Artiom12341 <70652258+Artiom12341@users.noreply.github.com>
2020-11-20 01:28:33 +0300

    Add files via upload

---
+++ informatica.inst.if.while.for.5.1.py
@@ -0,0 +1,25 @@
+n=eval(input("Dati anul="))
+if n%12==0 :
+    print("an.maimuta")
+elif n%12==1 :
+    print("an.cocos")
+elif n%12==2 :
+    print("an.ciine")
+elif n%12==3 :
+    print("an.porc")
+elif n%12==4 :
+    print("an.sobolan")
+elif n%12==5 :
+    print("an.bou")
+elif n%12==6 :
+    print("an.tigru")
+elif n%12==7 :
+    print("an.iepure")
+elif n%12==8 :
+    print("an.dragon")
+elif n%12==9:
+    print("an.sarpe")
+elif n%12==10 :
+    print("an.cal")
+elif n%12==11 :
+    print("an.oaie")
