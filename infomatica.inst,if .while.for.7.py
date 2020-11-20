55f7344684c8e54bec0c161ac9b7e7198256dddf
Artiom12341 <70652258+Artiom12341@users.noreply.github.com>
2020-11-20 21:51:01 +0200

    Update infomatica.inst,if .while.for.7.py

--- infomatica.inst,if .while.for.7.py
+++ infomatica.inst,if .while.for.7.py
@@ -1,14 +1,10 @@
 n=eval(input("dati n de ani ="))
-b=0
-c=1
 a=0
+c=1
 for i in range(1,n+1):
-    i=i+1
     c=c*2
-    b=c+i
-    a=a+b
-    s=1+a
+    a=c+i
 #Punct B
-    if s>100:
-        print("Varsta =",i-1)
-print("Nr. de dolari=",s,"$")
+if a>=100:
+    print("Varsta =",i)
+print("Nr. de dolari=",a,"$")
